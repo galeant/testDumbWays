7f4cecff0d2e1101135721eb1df71c79b311db69
ilham <ilham.fadillah@gmail.com>
2019-11-09 20:40:04 +0700

    6

--- 6.php
+++ 6.php
@@ -2,37 +2,30 @@
 
     $ar = [
         ['a','c','b','e','d'],
-        ['g','i','h','f'],
-        // ['p','v','w'],
-        ['z'],
-        // ['l','n']
+        ['g','e','f']
     ];
-    var_dump(sorting_array($ar));
+
+    $ar1 = [
+        ['g','h','i'],
+        ['a','c','b','e','d'],
+        ['g','e','f'],
+        ['z','d']
+    ];
+    var_dump(sorting_array($ar1));
 
     function sorting_array($ar){
-        foreach($ar as $index=>$are){
-            // $temp = []; 
-            for($i=0;$i<$index;$i++){
-                if(count($are) < count($ar[$i])){
-                    $temp = $ar[$i];
-                    $ar[$i] = $are;
-                    $ar[$index] = $temp;
-                    var_dump($i);
-                    var_dump($temp);
-                    var_dump($ar[$i]);
-                    var_dump($ar[$index]);
-                    die();
-                }
-            }
-            // sort($are);
-            
-            // foreach($are as $arel){
-            //     $temp[] = $arel;
-            // }
-            // $result[] = $temp;
+        $iar = [];
+        foreach($ar as $art){
+            $iar[] = count($art);
         }
-        var_dump($ar);die();
-        sort($result);
-        var_dump($result);die();
+        asort($iar);
+        $far = [];
+        foreach($iar as $ind=>$lar){
+            $far[] = $ar[$ind];
+        }
+        foreach($far as $flar){
+            sort($flar);
+        }
+        return $far;
     }
 ?>
